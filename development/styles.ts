34e88fa5f5ee28932217bb5dbbf3f522308f720d
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2025-07-01 23:11:57 +0100

    fix(styles): add dynamic padding for status bar height

--- development/styles.ts
+++ development/styles.ts
@@ -1,8 +1,11 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, StatusBar } from 'react-native';
+
+export const STATUSBAR_HEIGHT = StatusBar.currentHeight;
 
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white'
-    },
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingTop: STATUSBAR_HEIGHT,
+  },
 });
